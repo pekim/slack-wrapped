f27f5e862ae0a36e22e15ff71b0ca236feafb11f
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-04-26 12:57:28 +0100

    Generate source maps.

--- gulp/tasks/babel.js
+++ gulp/tasks/babel.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var config = require('../util/config');
 
 var glob = 'src/script/**/*.js';
 
 gulp.task('babel', function () {
-  return gulp
-    .src(glob, {base: config.path.source})
+  return gulp.src(glob, {base: config.path.source})
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(sourcemaps.write('map'))
     .pipe(gulp.dest(config.path.dest));
 });
 
