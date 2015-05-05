ebc1dc463a150fa0ebe98bd03e6f1bc2628643ba
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-05-05 21:16:07 +0100

    Filter list of electron release assets to just those of interest.

--- src/script/release/release.js
+++ src/script/release/release.js
@@ -3,32 +3,51 @@
 const async = require('async');
 const npm = require('npm');
 const request = require('request');
+const fs = require('fs');
+const path = require('path');
 
-const electronConfig = require('../../package.json').electron;
+const rootDir = '../../';
+const packageJsonPath = path.join(rootDir, 'package.json');
+
+const distDir = path.join('./', 'dist');
+const cacheDir = path.join(distDir, 'cache');
+
+const electronConfig = require(packageJsonPath).electron;
 
 const newVersion = process.argv[2];
 
-// console.log(newVersion);
-// console.log(electronConfig);
-
 function bumpNpmVersion(done) {
-  async.series([
-    callback => npm.load({}, callback),
-    // callback => npm.commands.version([newVersion], callback)
-  ], done);
+  if (newVersion) {
+    async.series([
+      callback => npm.load({}, callback),
+      callback => npm.commands.version([newVersion], callback)
+    ], done);
+  } else {
+    setImmediate(done);
+  }
 }
 
-function makeReleases(releaseTags, callback) {
-  const assets = releaseTags.assets
-    .filter(asset => asset.name.includes('linux-x64.zip'));
-  console.log(assets.length);
+function makeReleases(assets, callback) {
+  console.log(assets);
 
   callback();
 }
 
-function fetchReleaseTags(_, done) {
+function filterVariants(releaseTags, callback) {
+  const assets = releaseTags.assets
+    .filter(asset => {
+      return electronConfig.variants.some(variant => {
+        return asset.name.includes(variant) &&
+          !asset.name.includes('-symbols');
+      });
+    });
+
+  callback(null, assets);
+}
+
+function fetchReleaseTags(done) {
   request({
-    url    : 'https://api.github.com/repos/atom/electron/releases/tags/v0.25.1',
+    url    : `https://api.github.com/repos/atom/electron/releases/tags/v${electronConfig.version}`,
     headers: {
       'User-Agent': 'pekim'
     }
@@ -43,12 +62,29 @@ function fetchReleaseTags(_, done) {
   });
 }
 
+function mkdir(dir, done) {
+  fs.mkdir(dir, err => {
+    if (err && err.code !== 'EEXIST') {
+      done(err);
+    } else {
+      done();
+    }
+  });
+}
+
+function createDirectories(done) {
+  async.series([
+    callback => mkdir(distDir, callback),
+    callback => mkdir(cacheDir, callback)
+  ], (err) => done(err));   // discard result array
+}
+
 async.waterfall([
+  createDirectories,
   bumpNpmVersion,
-  fetchReleaseTags,
-  makeReleases
+  // fetchReleaseTags,
+  // filterVariants,
+  // makeReleases
 ], err => {
   console.error(err);
 });
-
-// https://api.github.com/repos/atom/electron/releases/tags/v0.25.1
