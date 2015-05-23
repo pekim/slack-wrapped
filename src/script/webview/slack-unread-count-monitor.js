3c6f126eab80ed401d9559484f649ae70bf44d7c
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-05-23 10:24:28 +0100

    Make listening for conversation unread count changes more readable.

--- src/script/webview/slack-unread-count-monitor.js
+++ src/script/webview/slack-unread-count-monitor.js
@@ -1,33 +1,32 @@
 'use strict';
-/* global TS */
-/* global unreadCountUpdate */
-/* eslint-disable no-var */
 
 // Based on the example in https://github.com/wlaurance/slack-for-linux/blob/master/app/js/index.js
 // (licensed under MIT)
 (function() {
-  var sig;
-
   if (!window.TS) {
     return;
   }
 
-  if (TS.channels) {
-    sig = TS.channels.unread_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
-    sig = TS.channels.unread_highlight_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
+  addConversationListeners(window.TS.channels);
+  addConversationListeners(window.TS.groups);
+  addConversationListeners(window.TS.ims);
+
+  if (window.TS.client) {
+    addListener(window.TS.client.login_sig);
   }
 
-  if (TS.groups) {
-    sig = TS.groups.unread_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
-    sig = TS.groups.unread_highlight_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
+  function addConversationListeners(conversations) {
+    if (!conversations) {
+      return;
+    }
+
+    addListener(conversations.unread_changed_sig);
+    addListener(conversations.unread_highlight_changed_sig);
   }
 
-  if (TS.ims) {
-    sig = TS.ims.unread_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
-    sig = TS.ims.unread_highlight_changed_sig; if (sig) { sig.add(unreadCountUpdate); }
-  }
-
-  if (TS.client) {
-    sig = TS.client.login_sig; if (sig) { sig.add(unreadCountUpdate); }
+  function addListener(sig) {
+    if (sig) {
+      sig.add(window.unreadCountUpdate);
+    }
   }
 })();
