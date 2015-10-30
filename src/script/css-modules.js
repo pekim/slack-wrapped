0494322195ca32736dfa8b7f64a098042fa6a38e
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-10-30 15:45:40 +0000

    Do nothing app, to prove babel and css-modules require hooks.

---
+++ src/script/css-modules.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import hook from 'css-modules-require-hook';
+import md5Hex from 'md5-hex';
+
+import postcssImport from 'postcss-import';
+import postcssModulesExtractImports from 'postcss-modules-extract-imports';
+import postcssModulesLocalByDefault from 'postcss-modules-local-by-default';
+import postcssModulesScope from 'postcss-modules-scope';
+
+function generateScopedName(importName, filepath) {
+  const basename = path.basename(filepath, '.css');
+  const hash = md5Hex(importName + filepath).substr(0, 6);
+
+  return `${basename}__${importName}__${hash}`;
+}
+
+function addStyleToHead(css) {
+  console.log(css);
+  // const headElement = document.querySelector('head');
+  // const styleElement = document.createElement('style');
+  //
+  // styleElement.type = 'text/css';
+  // styleElement.appendChild(document.createTextNode(css));
+  // headElement.appendChild(styleElement);
+
+  return css;
+}
+
+hook({
+  processCss: css => addStyleToHead(css),
+
+  use: [
+    postcssImport({
+      path: ['node_modules']
+    }),
+    postcssModulesExtractImports,
+    postcssModulesLocalByDefault,
+    postcssModulesScope({
+      generateScopedName: generateScopedName
+    })
+  ]
+});
---
+++ src/script/main.js
@@ -0,0 +1,4 @@
+import style from './test.css';
+
+console.log(new Date());
+console.log(style);
