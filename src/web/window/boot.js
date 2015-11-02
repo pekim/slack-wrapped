fa8e61d60b050cd6c721d788371d441415c454fe
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-11-02 09:26:59 +0000

    Pre-build instead of running babel at runtime (with a hook).
    - shaves over a second off startup time
    - do not show "Loading..." while starting up (transpiling)
    - add a bunch of npm scripts to do the build
    - build into a lib directory
    - still run postcss at runtime

--- src/web/window/boot.js
+++ src/web/window/boot.js
@@ -1,7 +1,5 @@
-require('babel-core/register');
-require('./css-modules-hook');
-
 // suppress annoying console message "Download the React DevTools..."
 window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {};
 
+require('./css-modules-hook');
 require('./window');
