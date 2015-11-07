535621cf3d431c5ea987743c457702c0edc0b13d
Mike D Pilsbury <mike.pilsbury@gmail.com>
2015-11-07 13:31:39 +0000

    Remove v1 files that are not required in v2.

--- src/script/slack-brand.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-exports.notAffiliatedText = 'not created by, affiliated with, or supported by Slack Technologies, Inc.';
-
-exports.getStickerImagePath = size => {
-  return path.join(__dirname, `/../image/slack-sticker-${size}.png`);
-};
